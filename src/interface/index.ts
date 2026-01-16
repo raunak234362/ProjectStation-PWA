76fdf5faf9050691671676f7023161db21e01bfd
Siddhi <siingh.siddhi@gmail.com>
2026-01-16 12:51:38 +0530

    Add fabric stage selection to AddFabricator component and update FabricatorPayload interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -76,13 +76,13 @@ export interface Department {
   createdAt?: string | Date;
   updatedAt?: string | Date;
   managerIds?:
-    | string
-    | []
-    | {
-        firstName?: string;
-        lastName?: string;
-        middleName?: string;
-      };
+  | string
+  | []
+  | {
+    firstName?: string;
+    lastName?: string;
+    middleName?: string;
+  };
 }
 
 export interface DepartmentPayload {
@@ -93,6 +93,7 @@ export interface FabricatorPayload {
   fabName: string;
   website?: string;
   drive?: string;
+  fabStage?: "RFQ" | "PRODUCTION";
   files?: File | string | "";
 }
 export interface FabricatorEditPayload {
